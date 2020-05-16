6e3d9f2be7d5aa7c51eb56bbdbfe9a4d92824d9b
Jack <giacomopappalardi@tiscali.it>
2020-05-16 14:55:00 +0200

    Aggiunta Cavallo.java Versione 1.0

---
+++ Cavallo.java
@@ -0,0 +1,57 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.MediaTracker;
+import java.awt.Toolkit;
+
+import javax.swing.JPanel;
+
+public class Cavallo extends JPanel {
+	int x;
+	int y;
+	Image img;
+
+	public Cavallo() {
+	
+		switch (xx) {
+		case 1: {
+			img = tk.getImage("D:/Eclipse Java/Giacomo/src/Cavallo1.jpg");
+			break;
+		}
+		case 2: {
+			img = tk.getImage("D:/Eclipse Java/Giacomo/src/Cavallo2.jpg");
+			break;
+		}
+		case 3: {
+			img = tk.getImage("D:/Eclipse Java/Giacomo/src/Cavallo3.jpg");
+			break;
+		}
+		case 4: {
+			img = tk.getImage("D:/Eclipse Java/Giacomo/src/Cavallo4.jpg");
+			break;
+		}
+		case 5: {
+			img = tk.getImage("D:/Eclipse Java/Giacomo/src/Cavallo5.jpg");
+			break;
+		}
+		case 6: {
+			img = tk.getImage("D:/Eclipse Java/Giacomo/src/Cavallo6.jpg");
+			break;
+		}
+		case 7: {
+			img = tk.getImage("D:/Eclipse Java/Giacomo/src/Cavallo7.jpg");
+			break;
+		}
+		case 8: {
+			img = tk.getImage("D:/Eclipse Java/Giacomo/src/Cavallo8.jpg");
+			break;
+		}
+		case 9: {
+			img = tk.getImage("D:/Eclipse Java/Giacomo/src/Cavallo9.jpg");
+			break;
+		}
+		case 10: {
+			img = tk.getImage("D:/Eclipse Java/Giacomo/src/Cavallo10.jpg");
+			break;
+		}
+		}
+		}
