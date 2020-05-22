74b0ece061b2f9f36aea0748870cce78c17a8e73
Jack <giacomopappalardi@tiscali.it>
2020-05-22 12:58:50 +0200

    aggiunta Cavallo.java 2.0

--- Cavallo.java
+++ Cavallo.java
@@ -2,6 +2,7 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.MediaTracker;
 import java.awt.Toolkit;
+import java.applet.*;
 
 import javax.swing.JPanel;
 
@@ -10,48 +11,72 @@ public class Cavallo extends JPanel {
 	int y;
 	Image img;
 
-	public Cavallo() {
-	
+	public Cavallo(int yy, int xx) {
+		x = 0;
+		y = yy;
+		setSize(80, 81);
+		Toolkit tk = Toolkit.getDefaultToolkit();
+
 		switch (xx) {
 		case 1: {
-			img = tk.getImage("D:/Eclipse Java/Giacomo/src/Cavallo1.jpg");
+			img = tk.getImage(getClass().getResource("Cavallo1.jpg"));
 			break;
 		}
 		case 2: {
-			img = tk.getImage("D:/Eclipse Java/Giacomo/src/Cavallo2.jpg");
+			img = tk.getImage(getClass().getResource("Cavallo2.jpg"));
 			break;
 		}
 		case 3: {
-			img = tk.getImage("D:/Eclipse Java/Giacomo/src/Cavallo3.jpg");
+			img = tk.getImage(getClass().getResource("Cavallo3.jpg"));
 			break;
 		}
 		case 4: {
-			img = tk.getImage("D:/Eclipse Java/Giacomo/src/Cavallo4.jpg");
+			img = tk.getImage(getClass().getResource("Cavallo4.jpg"));
 			break;
 		}
 		case 5: {
-			img = tk.getImage("D:/Eclipse Java/Giacomo/src/Cavallo5.jpg");
+			img = tk.getImage(getClass().getResource("Cavallo5.jpg"));
 			break;
 		}
 		case 6: {
-			img = tk.getImage("D:/Eclipse Java/Giacomo/src/Cavallo6.jpg");
+			img = tk.getImage(getClass().getResource("Cavallo6.jpg"));
 			break;
 		}
 		case 7: {
-			img = tk.getImage("D:/Eclipse Java/Giacomo/src/Cavallo7.jpg");
+			img = tk.getImage(getClass().getResource("Cavallo7.jpg"));
 			break;
 		}
 		case 8: {
-			img = tk.getImage("D:/Eclipse Java/Giacomo/src/Cavallo8.jpg");
+			img = tk.getImage(getClass().getResource("Cavallo8.jpg"));
 			break;
 		}
 		case 9: {
-			img = tk.getImage("D:/Eclipse Java/Giacomo/src/Cavallo9.jpg");
+			img = tk.getImage(getClass().getResource("Cavallo9.jpg"));
 			break;
 		}
 		case 10: {
-			img = tk.getImage("D:/Eclipse Java/Giacomo/src/Cavallo10.jpg");
+			img = tk.getImage(getClass().getResource("Cavallo10.jpg"));
 			break;
 		}
 		}
+		MediaTracker mt = new MediaTracker(this);
+		mt.addImage(img, 1);
+		try {
+			mt.waitForID(1);
+		} catch (Exception e) {
 		}
+	}
+	
+	public void setX(int n) {
+		x = n;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	
+	public void paint(Graphics g) {
+		g.drawImage(img,  x,  y, null);
+	}
+}
