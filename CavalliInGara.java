1048793f6a0ddea134271dc1f781b1b219f2383f
Jack <giacomopappalardi@tiscali.it>
2020-05-26 11:39:44 +0200

    aggiunta Overdrive a CavalliInGara

--- CavalliInGara.java
+++ CavalliInGara.java
@@ -1,3 +1,4 @@
+
 public class CavalliInGara implements Runnable {
 	Cavallo Cav;
 	GaraCavalli campo;
@@ -16,4 +17,23 @@ public class CavalliInGara implements Runnable {
 		posizione=0;
 	}
 	
+	@Override
+	public void run() {
+		int dimImmagine=79;
+		int dimPista=960;
+		//while((Cav.))
+		while((Cav.getX()+dimImmagine)<dimPista) {
+			if ((conta%10)==0)
+				velocita=(int)(Math.random()*10+1);
+			Cav.setX(Cav.getX()+velocita);
+			conta++;
+			try {Thread.sleep(75);}
+			catch (Exception e) {}
+			campo.repaint();
+		}
+		
+		posizione=campo.getPosizione();
+		campo.controllaArrivi();
+	}
 	
+}
